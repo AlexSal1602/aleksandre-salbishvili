29461f5cb8b24d2327fa24723f5fa67a883d5501
AlexSal1602 <asalbishvili@hotmail.com>
2023-08-24 12:16:32 +0400

    age always update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict"
 
 const startPage = (() => {
   const html = document.documentElement,
-        s = 'start-map'
+    s = "start-map"
   html.classList.add(s)
-  window.addEventListener('load', function() {
+  window.addEventListener("load", function () {
     setTimeout(() => {
       html.classList.remove(s)
     }, 2000) // the time must be at least equal to the duration of the CSS animation (personally I put a little more).
@@ -13,79 +13,68 @@ const startPage = (() => {
 
 //for remove loadder in 2 secs.
 
-const onLoadPage = document.querySelector('.onload-page')
+const onLoadPage = document.querySelector(".onload-page")
 
-window.addEventListener('load', function(){
-    setTimeout(function(){
-        onLoadPage.style.display= 'none';
-    }, 2000);
-  });
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    onLoadPage.style.display = "none"
+  }, 2000)
+})
 
-  document.querySelector('.my-photo').setAttribute('draggable', false);
+document.querySelector(".my-photo").setAttribute("draggable", false)
 
-  
-
-  
-  
-
+const myAgePlace = document.querySelector(".myAgePlace")
+const dt = new Date()
+const year = dt.getFullYear()
+const myAge = year - 1995
+myAgePlace.innerHTML = myAge
 
 //smoothed jump to id
 
-  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault()
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    })
+  })
+})
 
-$(document).ready(function(){
-  $(window).scrollTop(0);
-});
-
-
+$(document).ready(function () {
+  $(window).scrollTop(0)
+})
 
 //element slides on mousemove, could be it on scroll.
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll(".reveal")
 
   for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
+    var windowHeight = window.innerHeight
+    var elementTop = reveals[i].getBoundingClientRect().top
+    var elementVisible = 150
 
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      reveals[i].classList.add("active")
     } else {
-      reveals[i].classList.remove("active");
+      reveals[i].classList.remove("active")
     }
   }
 }
 
-
-
 setTimeout(() => {
-  window.addEventListener("mousemove", reveal);
+  window.addEventListener("mousemove", reveal)
 }, "2000")
 
-
-
-
 // Add active class to the current button (highlight it)
-var header = document.getElementById("mynav");
-var btns = header.getElementsByClassName("nav-item");
+var header = document.getElementById("mynav")
+var btns = header.getElementsByClassName("nav-item")
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active")
+    current[0].className = current[0].className.replace(" active", "")
+    this.className += " active"
+  })
 }
-
-
-
-
 
 //horizontal scroll
 /* let variation = 0;
@@ -98,20 +87,14 @@ for (var i = 0; i < btns.length; i++) {
     
 }, true); */
 
-
-
 // scroll to top
-window.onbeforeunload = function () { window.scrollTo(0,0); };
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0)
+}
 
-
-
-
-
-
-
-const mailClipboard = document.getElementById("mail");
+const mailClipboard = document.getElementById("mail")
 mailClipboard.addEventListener("click", () => {
   navigator.clipboard.writeText("asalbishvili@hotmail.com").then(() => {
-    alert("Email copied to clipboard");
-  });
-});
+    alert("Email copied to clipboard")
+  })
+})
